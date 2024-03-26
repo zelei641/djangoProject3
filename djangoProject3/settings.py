5e17bfb52485b427a3a7c2bb37453e0d94e5d12c
zelei <12632751+zelei111@user.noreply.gitee.com>
2024-03-26 17:27:47 +0800

    Initial commit

--- djangoProject3/settings.py
+++ djangoProject3/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-q=!7=(tucn0l!2u7w2l0y8rdp_$@e3o!x*)+s*cfqs=7xa3(@v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
